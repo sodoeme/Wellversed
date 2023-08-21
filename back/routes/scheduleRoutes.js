dc8f45afa4ab638cf9bf7119dc3bf28f52c2d5eb
OAKevin <kevinmirandat@gmail.com>
2023-08-21 14:11:36 -0400

    Added the Routes for the schedule

---
+++ back/routes/scheduleRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const productController = require('../controller/scheduleController')
+
+
+//return all schedules correlating to volunteer
+router.get('volunteer/:id', scheduleController.getAllSchedules)
+
+
+//return all schedules correlating to organization
+router.get('organization/:id', scheduleController.getAllSchedules)
+
+//create schedule
+
+router.post('/create', scheduleController.postSchedule)
+
+// add volunteer to schedule
+
+router.put('/pickup/:id', scheduleController.pickupVolunteer)
+
+// remove volunteer from schedule
+   
+router.put('/drop/:id', scheduleController.dropVolunteer)
+
+module.exports = router
