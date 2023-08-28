cc5ef38654ddb4f4f4a17f685ec23038a75b0238
sodoeme <88409744+sodoeme@users.noreply.github.com>
2023-08-28 14:26:06 -0400

    Update index.js

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter> {/* Wrap your App component with BrowserRouter */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
